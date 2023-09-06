db7fd0d804874fdf5fdafbca2eecb35774a2c1aa
jnhyk <hjh130101@gmail.com>
2023-09-06 10:25:52 +0900

    web workbench commit

--- server/Router/energyRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use(express.static("/root/workspace/hjh/KETI-Web-Workbench/client/build"));
-router.get("/", (req, res) => {
-  res.sendFile("Energy.js");
-});
-
-module.exports = router;
--- server/Router/workbenchRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use(express.static("/root/workspace/hjh/KETI-Web-Workbench/client/build"));
-router.get("/", (req, res) => {
-  res.sendFile("Workbench.js");
-});
-
-module.exports = router;
