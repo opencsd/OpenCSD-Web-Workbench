b719ab0e4b5fde6b6a5c98ba14362f9220e31a06
juhyunkil <wngus0331@gmail.com>
2023-10-17 17:21:02 +0900

    update

---
+++ static/js/query-controller-ssd.js
@@ -0,0 +1,96 @@
+let temp_id = 1;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const queryHistory = [];
+
+    document.getElementById("pushdownButton").addEventListener("click", function () {
+        const queryText = document.getElementById("queryTextarea").value.trim();
+
+        const queryLogTableBody = document.getElementById("queryLogTableBody");
+
+        if (queryText) {
+            const shortenedQuery = queryText.length > 70 ? queryText.slice(0, 70) + "..." : queryText;
+            queryHistory.push(shortenedQuery);
+
+            const newRow = document.createElement("tr");
+            const checkboxCell = document.createElement("td");
+            checkboxCell.style.width = "5%";
+            const queryIDCell = document.createElement("td");
+            queryIDCell.style.width = "1%";
+            const queryCell = document.createElement("td");
+            queryCell.style.width = "50%";
+            const progressBarCells = [];
+            const dummyButtonCell = document.createElement("td");
+            dummyButtonCell.style.width = "5%";
+
+            for (let i = 0; i < 2; i++) {
+                const progressBarCell = document.createElement("td");
+                progressBarCell.style.width = "12%";
+                let width;
+                let value;
+                let max;
+                let label;
+
+                if(i==0){
+                    value = 101250;
+                    max = 200000;
+                }else{
+                    value = 14;
+                    max = 100;
+                }
+
+                label = value;
+                width = (value / max) * 100;
+                progressBarCell.innerHTML = `
+                    <div class="progress">
+                        <div class="progress-bar" style="width: ${width}%" role="progressbar" aria-valuenow="${value}"
+                            aria-valuemin="0" aria-valuemax="${max}" aria-label="${label}% Complete">
+                            
+                        </div>
+                    </div>
+                    <div style="text-align:center">
+                        <h6 style="margin-top:5px; margin-bottom:0px;">${value}</h6>
+                    </div>
+                `;
+                progressBarCells.push(progressBarCell);
+            }
+
+            const dummyButton = document.createElement("td");
+            dummyButton.innerHTML = `
+                <button class="btn btn-link p-0 ssd_btn" id="queryLogDetail">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                    </svg>
+                </button>
+            `;
+
+            checkboxCell.innerHTML = `<input type="checkbox" class="form-check-input" name="form-type[]" value="1">`;
+            newRow.appendChild(checkboxCell);
+
+            queryIDCell.textContent = temp_id;
+            temp_id++;
+            queryIDCell.style.textAlign = "center";
+            newRow.appendChild(queryIDCell);
+
+            queryCell.textContent = `${shortenedQuery}`;
+            queryCell.style.backgroundColor = "#fcfdfe";
+            newRow.appendChild(queryCell);
+
+            for (let i = 0; i < 2; i++) {
+                newRow.appendChild(progressBarCells[i]);
+            }
+
+            newRow.appendChild(dummyButtonCell);
+            dummyButtonCell.appendChild(dummyButton);
+
+            setTimeout(() => {
+                queryLogTableBody.appendChild(newRow);
+            }, 2000); 
+            
+        } else {
+            queryLogTableBody.textContent = "No query available.";
+        }
+
+        // document.getElementById("queryTextarea").value = "";
+    });
+});
--- static/js/query-ssd.js
+++ static/js/query-ssd.js
@@ -1,3 +1,5 @@
+let intervalId;
+
 const environmentInfoTab = document.querySelector("#environmentInfoTab.ssd_btn");
 const dbSchemaTab = document.querySelector("#dbSchemaTab.ssd_btn");
 
@@ -23,205 +25,251 @@ dbSchemaTab.addEventListener("click", function() {
     envSetting.style.display = "none";
 });
 
+const metricViewBtn = document.getElementById('metricViewBtn');
+const metricViewPlayIcon = document.getElementById('metricViewPlayIcon');
+const metricViewPauseIcon = document.getElementById('metricViewPauseIcon');
+let isMetricViewBtnClicked = true;
 
-document.addEventListener("DOMContentLoaded", function () {
-    window.ApexCharts &&
-        new ApexCharts(document.getElementById("query_cache1"), {
-            chart: {
-                type: "area",
-                fontFamily: "inherit",
-                height: 240,
-                parentHeightOffset: 0,
-                toolbar: {
-                    show: false,
-                },
-                animations: {
-                    enabled: false,
-                },
-                stacked: false,
-            },
-            plotOptions: {
-                bar: {
-                    barHeight: "50%",
-                    horizontal: false,
-                },
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            fill: {
-                opacity: 1,
-            },
-            series: [{
-                    name: "Strange sunglasses",
-                    data: [11, 13, 10, 12, 11, 15, 19, 12, 11, 15, 17, 12],
-                }
-            ],
-            colors: ["#77c13d"],
-            tooltip: {
-                theme: "dark",
-            },
-            grid: {
-                padding: {
-                    top: -20,
-                    right: 0,
-                    left: 20,
-                    bottom: -4,
-                },
-                strokeDashArray: 4,
-            },
-            xaxis: {
-                labels: {
-                    padding: 0,
-                },
-                tooltip: {
-                    enabled: false,
-                },
-                axisBorder: {
-                    show: false,
-                },
-                categories: [
-                    "15:00",
-                    "",
-                    "15:10",
-                    "",
-                    "15:20",
-                    "",
-                    "15:30",
-                    "",
-                    "15:40",
-                    "",
-                    "15:50",
-                    "",
-                ],
-            },
-            yaxis: {
-                tickAmount: 6,
-                min: 0,
-                max: 30
-            },
-            legend: {
-                show: false
-            },
-            stroke: {
-                curve: 'straight' 
-            },
-        }).render();
+metricViewBtn.addEventListener('click', () => {    
+    if (isMetricViewBtnClicked) {
+        metricViewPlayIcon.style.display = 'inline';
+        metricViewPauseIcon.style.display = 'none';
+        clearInterval(intervalId);
+    } else {
+        metricViewPlayIcon.style.display = 'none';
+        metricViewPauseIcon.style.display = 'inline';
+        startInterval();
+    }
+    isMetricViewBtnClicked = !isMetricViewBtnClicked;
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    window.ApexCharts &&
-        new ApexCharts(document.getElementById("query_cache2"), {
-            chart: {
-                type: "area",
-                fontFamily: "inherit",
-                height: 240,
-                parentHeightOffset: 0,
-                toolbar: {
-                    show: false,
-                },
-                animations: {
-                    enabled: false,
-                },
-                stacked: false,
+function updateChart(){
+    const hostServerCPUChart = new ApexCharts(document.getElementById("query_cache1"), {
+        chart: {
+            type: "area",
+            fontFamily: "inherit",
+            height: 240,
+            parentHeightOffset: 0,
+            toolbar: {
+                show: false,
             },
-            plotOptions: {
-                bar: {
-                    barHeight: "50%",
-                    horizontal: false,
-                },
-            },
-            dataLabels: {
+            animations: {
                 enabled: false,
             },
-            fill: {
-                opacity: 1,
+            stacked: false,
+        },
+        plotOptions: {
+            bar: {
+                barHeight: "50%",
+                horizontal: false,
             },
-            series: [{
-                    name: "Strange sunglasses",
-                    data: [11, 13, 10, 12, 11, 15, 10, 12, 17, 11, 15, 12],
-                }
-            ],
-            colors: ["#77c13d"],
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        fill: {
+            opacity: 1,
+        },
+        series: [{
+                name: "Strange sunglasses",
+                data: [11, 13, 10, 12, 11, 15, 19, 12, 11, 15, 17, 12],
+            }
+        ],
+        colors: ["#77c13d"],
+        tooltip: {
+            theme: "dark",
+        },
+        grid: {
+            padding: {
+                top: -20,
+                right: 0,
+                left: 20,
+                bottom: -4,
+            },
+            strokeDashArray: 4,
+        },
+        xaxis: {
+            labels: {
+                padding: 0,
+            },
             tooltip: {
-                theme: "dark",
+                enabled: false,
             },
-            grid: {
-                padding: {
-                    top: -20,
-                    right: 0,
-                    left: 20,
-                    bottom: -4,
-                },
-                strokeDashArray: 4,
+            axisBorder: {
+                show: false,
             },
-            xaxis: {
-                labels: {
-                    padding: 0,
-                },
-                tooltip: {
-                    enabled: false,
-                },
-                axisBorder: {
-                    show: false,
-                },
-                categories: [
-                    "15:00",
-                    "",
-                    "15:10",
-                    "",
-                    "15:20",
-                    "",
-                    "15:30",
-                    "",
-                    "15:40",
-                    "",
-                    "15:50",
-                    "",
-                ],
+            categories: [
+                "15:00",
+                "",
+                "15:10",
+                "",
+                "15:20",
+                "",
+                "15:30",
+                "",
+                "15:40",
+                "",
+                "15:50",
+                "",
+            ],
+        },
+        yaxis: {
+            tickAmount: 6,
+            min: 0,
+            max: 30
+        },
+        legend: {
+            show: false
+        },
+        stroke: {
+            curve: 'straight' 
+        },
+    });
+
+    const hostServerPowerChart = new ApexCharts(document.getElementById("query_cache2"), {
+        chart: {
+            type: "area",
+            fontFamily: "inherit",
+            height: 240,
+            parentHeightOffset: 0,
+            toolbar: {
+                show: false,
             },
-            yaxis: {
-                tickAmount: 6,
-                min: 0,
-                max: 30
+            animations: {
+                enabled: false,
             },
-            legend: {
-                show: false
+            stacked: false,
+        },
+        plotOptions: {
+            bar: {
+                barHeight: "50%",
+                horizontal: false,
             },
-            stroke: {
-                curve: 'straight' 
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        fill: {
+            opacity: 1,
+        },
+        series: [{
+                name: "Strange sunglasses",
+                data: [11, 13, 10, 12, 11, 15, 10, 12, 17, 11, 15, 12],
+            }
+        ],
+        colors: ["#77c13d"],
+        tooltip: {
+            theme: "dark",
+        },
+        grid: {
+            padding: {
+                top: -20,
+                right: 0,
+                left: 20,
+                bottom: -4,
             },
-        }).render();
+            strokeDashArray: 4,
+        },
+        xaxis: {
+            labels: {
+                padding: 0,
+            },
+            tooltip: {
+                enabled: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            categories: [
+                "15:00",
+                "",
+                "15:10",
+                "",
+                "15:20",
+                "",
+                "15:30",
+                "",
+                "15:40",
+                "",
+                "15:50",
+                "",
+            ],
+        },
+        yaxis: {
+            tickAmount: 6,
+            min: 0,
+            max: 30
+        },
+        legend: {
+            show: false
+        },
+        stroke: {
+            curve: 'straight' 
+        },
+    });
+
+    hostServerCPUChart.render();
+    hostServerPowerChart.render();
+}
+
+function startInterval(){
+    intervalId = setInterval(updateChart,1000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    startInterval();
 });
 
+const resultContainer = document.getElementById("resultContainer");
+const metricContainer = document.getElementById("metricContainer");
+const spinnerContainer = document.querySelectorAll(".spinnerContainer");
 
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("pushdownButton").addEventListener("click", function () {
+        spinnerContainer.forEach((icon) => {
+            icon.style.display = "flex";
+        });
+        resultContainer.style.display = "none";
+        metricContainer.style.display = "none";  
 
-        const scannedtable1 = document.querySelector('td.qtable_1');
-        const scannedtable2 = document.querySelector('td.qtable_2');
-        const scannedtable3 = document.querySelector('td.qtable_3');
+        setTimeout(() => {
+            spinnerContainer.forEach((icon) => {
+                icon.style.display = "none";
+            });
+            resultContainer.style.display = "block";
+            metricContainer.style.display = "block";
 
-        scannedtable1.textContent = "300 (line)";
-        scannedtable2.textContent = "20 (sec)";
-        scannedtable3.textContent = "4";
+            const scannedtable1 = document.querySelector('td.qtable_1');
+            const scannedtable2 = document.querySelector('td.qtable_2');
+            const scannedtable3 = document.querySelector('td.qtable_3');
 
-        var query = document.getElementById("queryTextarea").value;
-        var result = "+-------------------------------------+\n" +
-            "|ps_partkey        |value             |\n" +
-            "+-------------------------------------+\n" +
-            "|4877              |18980009.120000   |\n" +
-            "|198585            |16694701.690000   |\n" +
-            "|78280             |15889749.480000   |\n" +
-            "|89702             |15676712.640000   |\n" +
-            "|15055             |15452319.200000   |\n" +
-            "+-------------------------------------+";
-        document.getElementById("queryResult").value = result;
+            scannedtable1.textContent = "300 (line)";
+            scannedtable2.textContent = "20 (sec)";
+            scannedtable3.textContent = "4";
+
+            var result = "+-----------------------------------+\n" +
+                "|ps_partkey      |value                        |\n" +
+                "+-----------------------------------+\n" +
+                "|4877                |18980009.120000   |\n" +
+                "|198585            |16694701.690000   |\n" +
+                "|78280              |15889749.480000   |\n" +
+                "|89702              |15676712.640000   |\n" +
+                "|15055              |15452319.200000   |\n" +
+                "+-----------------------------------+";
+            document.getElementById("queryResult").value = result;
+
+            metricViewPlayIcon.style.display = 'inline';
+            metricViewPauseIcon.style.display = 'none';
+            isMetricViewBtnClicked = true;
+            clearInterval(intervalId);
+        }, 20000); 
     });
 });
 
 const queryNumbers = Array.from({ length: 22 }, (_, i) => i + 1);
 const dropdownMenu = document.querySelector(".dropdown-menu");
+const queryTextArea = document.getElementById("queryTextarea");
+const dropdownToggle = document.getElementById("dropdownToggle");
 
 queryNumbers.forEach((number) => {
     const dropdownItem = document.createElement("a");
@@ -230,13 +278,9 @@ queryNumbers.forEach((number) => {
     dropdownItem.textContent = `Q${number}`;
     dropdownMenu.appendChild(dropdownItem);
 
-    const dropdownToggle = document.getElementById("dropdownToggle");
-const dropdownItems = document.querySelectorAll(".dropdown-item");
-
-    dropdownItems.forEach((dropdownItem) => {
     dropdownItem.addEventListener("click", function (event) {
         event.preventDefault(); 
-                dropdownToggle.textContent = dropdownItem.textContent;
-    }); 
-    });
+            dropdownToggle.textContent = dropdownItem.textContent;
+            queryTextArea.value = "SELECT\n\t ps_partkey,Sum(ps_supplycost * ps_availqty) AS value\n FROM\n\t   partsupp, supplier,nation\n WHERE\n\t  ps_suppkey = s_suppkey AND\n\t s_nationkey = n_nationkey AND\n\t n_name = 'MOZAMBIQUE'\n GROUP  BY\n\t ps_partkey\n HAVING\n\t Sum(ps_supplycost * ps_availqty) > (\n\tSELECT\n\t\t Sum(ps_supplycost * ps_availqty) * 0.0001000000\n\t FROM\n\t\t   partsupp, supplier, nation\n\t  WHERE\n\t\t  ps_suppkey = s_suppkey AND\n\t\t s_nationkey = n_nationkey AND\n\t\t n_name = 'MOZAMBIQUE'\n\t)\n ORDER  BY value DESC; ";
+}); 
 });
